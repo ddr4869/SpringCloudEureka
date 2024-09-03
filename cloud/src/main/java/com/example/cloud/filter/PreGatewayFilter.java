9e047d20f9c389ca9d25dbc04e5b2eea01dde297
ddr4869 <bikdu@naver.com>
2024-09-03 13:26:51 +0900

    feat: Logging filter apply

---
+++ cloud/src/main/java/com/example/cloud/filter/PreGatewayFilter.java
@@ -0,0 +1,35 @@
+package com.example.cloud.filter;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class PreGatewayFilter extends AbstractGatewayFilterFactory<PreGatewayFilter.Config> {
+
+    public PreGatewayFilter() {
+        super(Config.class);
+    }
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            log.info("request information : {}, {}", request.getHeaders(), request.getURI());
+
+            ServerHttpRequest.Builder builder = exchange.getRequest().mutate();
+
+            return chain.filter(exchange.mutate().request(builder.build()).build());
+        };
+    }
+
+    @Getter
+    @Setter
+    public static class Config {
+        // 필터와 관련된 설정값을 추가할 수 있음
+    }
+}
