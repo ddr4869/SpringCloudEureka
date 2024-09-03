a6cf5f2b1cd6fc510b8aabb684803d84a895303b
ddr4869 <bikdu@naver.com>
2024-09-03 12:55:59 +0900

    feat: Add Api-gateway & Eureka Server

---
+++ first-service/src/main/java/com/example/first_service/controller/Controller.java
@@ -0,0 +1,32 @@
+package com.example.first_service.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/first-service")
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Welcome to the first service.";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        log.info("Server port = {}", request.getServerPort());
+        return String.format("This is a message from First Service on PORT %s",
+                8085);
+    }
+}
