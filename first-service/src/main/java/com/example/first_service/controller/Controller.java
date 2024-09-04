47c00914cc6b295cec1429e327b9c22df38cee5d
ddr4869 <bikdu@naver.com>
2024-09-04 10:14:51 +0900

    feat: Kafka testing

--- first-service/src/main/java/com/example/first_service/controller/Controller.java
+++ first-service/src/main/java/com/example/first_service/controller/Controller.java
@@ -1,20 +1,25 @@
 package com.example.first_service.controller;
 
+import com.example.first_service.kafka.KafkaProducerController;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
+@AllArgsConstructor
 public class Controller {
+
+    private final Environment env;
+    private final KafkaProducerController kafkaProducerController;
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome to the First service.";
+        return String.format("Welcome to the First Service, Port is %s",
+                 env.getProperty("token.secret"));
     }
 
     @GetMapping("/message")
@@ -28,5 +33,9 @@ public class Controller {
         log.info("Server port = {}", request.getServerPort());
         return String.format("This is a message from First Service on PORT %s",
                 8081);
+    }
+    @PostMapping("/kafka")
+    public void kafka(HttpServletRequest request) {
+        kafkaProducerController.sendMassage("Hello Kafka");
     }
 }
